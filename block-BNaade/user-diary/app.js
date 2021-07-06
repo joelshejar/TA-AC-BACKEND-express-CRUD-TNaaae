685d8b5a689b758124b7e3439b37d2ea3ba8df5e
abc <joel.rajesh13@gmail.com>
2021-07-06 19:04:37 +0530

    Update in CRUD

---
+++ block-BNaade/user-diary/app.js
@@ -0,0 +1,32 @@
+var express = require('express')
+var path = require('path')
+var usersRouter = require('./routes/users')
+var mongoose = require('mongoose')
+mongoose.connect("mongodb://localhost/user-diary",{useNewUrlParser:true,useUnifiedTopology:true},
+(err)=>{
+    console.log(err?"connected false":"connected true")
+})
+var app = express()
+app.set('view-engine', 'ejs')
+app.set('views', path.join(__dirname, "views"))
+app.use(express.urlencoded({extended:false}))
+app.get('/',(req,res)=>{
+    res.render('index.ejs')
+})
+app.use('/users',usersRouter)
+
+// handle error
+
+app.use((req,res,next)=>{
+    res.status(404).send('Page not found')
+})
+
+app.use((err,req,res,next)=>{
+    res.send(err)
+})
+app.listen(3000,()=>{
+    console.log('listening to port 3000')
+})
+
+
+
---
+++ block-BNaade/user-diary/models/User.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose')
+var Schema = mongoose.Schema
+var userSchema = new Schema({
+    name:String,
+    email:String,
+    age:Number
+},{timestamps:true})
+
+
+module.exports = mongoose.model("User", userSchema)
